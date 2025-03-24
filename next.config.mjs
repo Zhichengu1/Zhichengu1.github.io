9a2016e7642b2451d337a2f80f60613323d65748
Zhicheng Yu <yuzhicheng369@gmail.com>
2025-03-23 20:43:46 -0400

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-// next.config.mjs
-const nextConfig = {
-    output: 'export',
-    basePath: process.env.NODE_ENV === 'production' ? '/Zhichengu1.github.io' : '',
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
